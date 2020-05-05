6d8b515472f72b3a1e8465ce19f65eabca3711b5
Jared Markell <982083+jaredm4@users.noreply.github.com>
2020-05-05 11:14:27 -0700

    Dependency injection for services.

--- src/Command/HelloWorldCommand.php
+++ src/Command/HelloWorldCommand.php
@@ -6,21 +6,19 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class HelloWorld extends Command
+class HelloWorldCommand extends Command
 {
-    protected static string $defaultName = 'mud:hello-world';
+    protected static string $defaultName = 'test';
 
     protected function configure()
     {
         $this
-            ->setDescription('Hello world sample to ensure configuration is setup correctly.')
-            ->setHelp('Not much to see here.');
+            ->setDescription('Hello world sample to ensure configuration is setup correctly.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('Hello, world.');
-
         return 0;
     }
 }
